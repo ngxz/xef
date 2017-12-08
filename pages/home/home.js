58d81115d701b6ee3aa53d67fc64f1839239cf0a
ngxz <ngxz92@163.com>
2017-12-08 18:25:01 +0800

    后台修改到小程序

---
+++ pages/home/home.js
@@ -0,0 +1,74 @@
+// pages/home/home.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+    orderI:'../../img/order_i.png',
+    goodsI:'../../img/goods_i.png',
+    payI:'../../img/pay_i2.png',
+    tableI:'../../img/table_i.png',
+    codeI:'../../img/code_i2.png',
+    manaI:'../../img/mana_i.png',
+    boxI:'../../img/box_i.png',
+    soI:'../../img/so_i.png',
+    lineI:'../../img/line_03_01.png'
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+  
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+  
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+  
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+  
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+  
+  }
+})
